d1608ecf1b44c902f3b6aa7d5caff7ca77d7a68e
wenjing <wenjing1473909841@163.com>
2022-06-01 10:46:44 +0800

    feat: 部分 06 如何使用 Dev Server 提高本地开发效率

---
+++ Webpack/06/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  }
+};
+
